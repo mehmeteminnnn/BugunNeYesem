ae58b67c43af00ee0dc0ad67fcbc5244df788a89
Mehmet Emin TOK <95161664+mehmeteminnnn@users.noreply.github.com>
2023-03-09 01:01:32 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -36,6 +38,27 @@ class _YemekSayfasiState extends State<YemekSayfasi> {
   int corbano = 1;
   int yemekno = 1;
   int tatlino = 1;
+  List<String> CorbaAdlari = [
+    "Mercimek",
+    "Tarhana",
+    "Tavuk Suyu",
+    "Düğün",
+    "Yüksük"
+  ];
+  List<String> YemekAdlari = [
+    "Karnıyarık",
+    "Mantı",
+    "Fasulye",
+    "İçli Köfte",
+    "Balık"
+  ];
+  List<String> TatliAdlari = [
+    "Kadayıf",
+    "Baklava",
+    "Sütlaç",
+    "Kazandibi",
+    "Dondurma"
+  ];
   Widget build(BuildContext context) {
     return Center(
       child: Column(children: [
@@ -46,11 +69,20 @@ class _YemekSayfasiState extends State<YemekSayfasi> {
                 style: ButtonStyle(
                     overlayColor: MaterialStatePropertyAll(Colors.transparent)),
                 onPressed: () {
+                  YemekDegis();
                   print("Corbaya tiklandi!");
                 },
                 child: Image.asset("assets/corba_$corbano.jpg")),
           ),
         ),
+        Text(CorbaAdlari[(corbano) - 1], style: TextStyle(fontSize: 19)),
+        Container(
+          width: 160,
+          child: Divider(
+            height: 5,
+            color: Colors.black,
+          ),
+        ),
         Expanded(
           child: Padding(
             padding: const EdgeInsets.all(11.0),
@@ -58,11 +90,20 @@ class _YemekSayfasiState extends State<YemekSayfasi> {
                 style: ButtonStyle(
                     overlayColor: MaterialStatePropertyAll(Colors.transparent)),
                 onPressed: () {
+                  YemekDegis();
                   print("Ana yemeğe tiklandi!");
                 },
                 child: Image.asset("assets/yemek_$yemekno.jpg")),
           ),
         ),
+        Text(YemekAdlari[(yemekno) - 1], style: TextStyle(fontSize: 19)),
+        Container(
+          width: 160,
+          child: Divider(
+            height: 5,
+            color: Colors.black,
+          ),
+        ),
         Expanded(
           child: Padding(
             padding: const EdgeInsets.all(11.0),
@@ -70,12 +111,30 @@ class _YemekSayfasiState extends State<YemekSayfasi> {
                 style: ButtonStyle(
                     overlayColor: MaterialStatePropertyAll(Colors.transparent)),
                 onPressed: () {
-                  print("Tatlıya yiklandi!");
+                  YemekDegis();
+
+                  print("Tatlıya tiklandi!");
                 },
                 child: Image.asset("assets/tatli_$tatlino.jpg")),
           ),
         ),
+        Text(TatliAdlari[(tatlino) - 1], style: TextStyle(fontSize: 19)),
+        Container(
+          width: 160,
+          child: Divider(
+            height: 5,
+            color: Colors.black,
+          ),
+        ),
       ]),
     );
+  }
+
+  void YemekDegis() {
+    setState(() {
+      corbano = (Random().nextInt(5)) + 1;
+      tatlino = (Random().nextInt(5)) + 1;
+      yemekno = (Random().nextInt(5)) + 1;
+    });
   }
 }
